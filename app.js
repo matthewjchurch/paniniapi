cc02e36773433e62fc7397110f6b330f8514f1b6
matthewjchurch <matthewjchurch@gmail.com>
2020-11-09 08:47:44 +0000

    first working commit

---
+++ app.js
@@ -0,0 +1,37 @@
+import http from 'http';
+import cors from 'cors';
+import express from 'express';
+import bodyParser from "body-parser";
+
+import { getCollectionDocuments } from "./db.js";
+import { createCollectionDocument } from "./db.js";
+import { updateCollectionDocument } from "./db.js";
+import { deleteCollectionDocument } from "./db.js";
+
+// 1. Create our express API
+const app = express();
+
+// NOTE: let's allow requests from ALL ORIGINS
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', async (request, response) => {
+
+    const getPlayers = await getCollectionDocuments('players');
+    response.send(getPlayers[Math.floor(Math.random() * Math.floor(getPlayers.length))]);
+    // response.send(getPlayers)
+})
+
+app.post('/create', async (request, response) => {
+    const createPlayer = request.body;
+    const posting = await createCollectionDocument('players', createPlayer);
+    response.send(createPlayer);
+})
+
+app.delete('/delete', async (request, response) => {
+    const deletePlayer = await deleteCollectionDocument('players', request.body.name)
+    response.send({response: 'delete successful'});
+})
+
+// 3. Finally! Listen on Port 8080
+app.listen(8080);
---
+++ db.js
@@ -0,0 +1,68 @@
+// Use ES6 Imports for mongodb and our mongo client
+import dotenv from 'dotenv';
+dotenv.config();
+import mongodb from 'mongodb';
+const mongoClient = mongodb.MongoClient;
+
+console.log(process.env.DB_USERNAME);
+// let dbName = "players";
+let dbName = "players";
+// connection string
+// const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@trees.bnyoy.mongodb.net/vulis_db?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xmfro.mongodb.net/players?retryWrites=true&w=majority`
+export const getCollectionDocuments = async (collectionName) => {
+    // Connect to our database / open our connection
+    const mongo = await mongoClient.connect(uri, { useUnifiedTopology: true })
+    // Retrieve our collection
+    const dataCollection = await mongo.db(dbName).collection(collectionName).find({}).toArray();
+    // Close our connection
+    mongo.close();
+    return dataCollection;
+}
+export const createCollectionDocument = async (collectionName, data) => {
+    // Connect to our database / open our connection
+    const mongo = await mongoClient.connect(uri, { useUnifiedTopology: true })
+    // const existingPlayer = await mongo.db('players').collection('players').find({data});
+    // console.log(existingPlayer);
+    // Create our document
+    // if (!existingPlayer){
+        if (!data._id) {
+            data._id = new mongodb.ObjectID().toString();
+            await mongo.db(dbName).collection(collectionName).insertOne(data)
+        } else {
+            updateCollectionDocument(collectionName, data);
+        } 
+    // } else {
+    //     response.send({response: "player already exists"})
+    // }
+    // Close our connection
+    mongo.close();
+}
+export const updateCollectionDocument = async (collectionName, data) => {
+    // Connect to our database / open our connection
+    const mongo = await mongoClient.connect(uri, { useUnifiedTopology: true })
+    // Retrieve our collection
+    var myquery = { _id: new mongodb.ObjectID(data._id) };
+    var newvalues = { $set: data };
+    await mongo.db(dbName).collection(collectionName).replaceOne(
+        { _id : data._id },
+        data, 
+        { upsert: true} 
+    );
+    // Close our connection
+    mongo.close();
+}
+export const deleteCollectionDocument = async (collectionName, data) => {
+    // Connect to our database / open our connection
+    const mongo = await mongoClient.connect(uri, { useUnifiedTopology: true })
+    // Retrieve our collection
+    try {
+        await mongo.db(dbName).collection(collectionName).deleteOne(
+            { name : data }
+        );
+    } catch(e) {
+        console.log(e);
+    }
+    // Close our connection
+    mongo.close();
+}
